21ddad77123cbf07fdf7146588e3c9e866cd1ea3
Mahmoud Kanani <mahmoud.kanani@damacgroup.com>
2020-03-13 19:40:26 +0400

    Add Polyfill

--- src/pages/ArticlePage.js
+++ src/pages/ArticlePage.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import ArticlesList from '../components/ArticlesList';
 import NotFoundPage from './NotFoundPage';
 import articleContent from './article-content';
@@ -7,6 +7,14 @@ const ArticlePage = ({ match }) => {
     const name = match.params.name;
     const article = articleContent.find(article => article.name === name);
 
+    const [articleInfo, setArticleInfo] = useState( /*initial value for articleInfo before we load any data or change to state */ { upvotes: 0, comments: [] });
+
+    // the function that we pass to *useEffect* will get called whenever this component is loaded
+    // useEffect hook runs not only when the component first mounts, but also whenever the component updates (in our case, if articleInfo changes)
+    useEffect(() => {
+        setArticleInfo({ upvotes: Math.ceil(Math.random() * 10) });
+    }, [name]);
+
     if (!article) return <NotFoundPage />
 
     const otherArticles = articleContent.filter(article => article.name !== name);
@@ -14,6 +22,7 @@ const ArticlePage = ({ match }) => {
     return (
         <>
         <h1>{article.title}</h1>
+        <p>This post has been upvoted {articleInfo.upvotes} times</p>
         {article.content.map((paragraph, key) => (
             <p key={key}>{paragraph}</p>
         ))}
